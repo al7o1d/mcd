254236ebb172d2cd80e34d3130d47a360cade560
al7o1d <n1mr0d@abv.bg>
2013-12-11 15:41:45 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mcdapp::Application.config.secret_key_base = '0e0872743faf17ad49cd45cd4cd9e65808e22aff6f9ee2f7222d6de18ea6bc91d9df167f0ea098114bc79ebd5436da196af880b7c7f6151c15c55152666cafe3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mcdapp::Application.config.session_store :cookie_store, key: '_mcdapp_session'
