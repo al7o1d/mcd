12704004edc3330878e2da875f47d6881390af02
al7o1d <n1mr0d@abv.bg>
2013-12-11 17:25:37 +0000

    added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ActiveRecord::Base
 	validates :city, length: { maximum: 30 }
 	validates :cost, numericality: { greater_than: 0.01 }
+	validates :city, :cost, presence: true
 end
