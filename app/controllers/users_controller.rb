a76f95c714bb148dcb2a76c9d6583cac9e872ada
al7o1d <n1mr0d@abv.bg>
2014-01-02 16:57:52 +0000

    done sign in and sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
+	    sign_in @user
 	    flash[:success] = "Signed Up! Welcome to the mcdprice Application!"
       redirect_to @user
     else
